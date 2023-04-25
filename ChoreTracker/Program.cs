aab3de8caa99a974937421f89c5a2b6deea04a9d
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-04-26 06:26:24 +0800

    create switch structure

--- ChoreTracker/Program.cs
+++ ChoreTracker/Program.cs
@@ -4,6 +4,23 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
+        string option = args[0].ToLower();
+        switch (option)
+        {
+            case "add": break;
+            case "remove": break;
+            case "do": break;
+            case "list": break;
+            case "last": break;
+            case "--help":
+            case "-h":
+            case "help":
+                // help section appears
+                break;
+            default:
+                Console.WriteLine("No such option!");
+                // help section appears
+                break;
+        }
     }
 }
