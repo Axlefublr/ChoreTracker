b849aa7c63ac8e9f3b0f44bfafe9aaf2ffee631c
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-05-14 14:58:34 +0800

    change to tabs

--- ChoreTracker/ChoreManager.cs
+++ ChoreTracker/ChoreManager.cs
@@ -8,122 +8,122 @@ namespace ChoreTracker;
 public class ChoreManager
 {
 
-    private readonly string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "chores.json");
+	private readonly string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "chores.json");
 
-    private readonly JsonObject jsonObj;
+	private readonly JsonObject jsonObj;
 
-    public ChoreManager()
-    {
-        MakeSureExists();
-        jsonObj = Parse();
-    }
+	public ChoreManager()
+	{
+		MakeSureExists();
+		jsonObj = Parse();
+	}
 
-    public void AddRange(string[] chores)
-    {
-        foreach (string chore in chores)
-        {
-            Add(chore);
-        }
-    }
+	public void AddRange(string[] chores)
+	{
+		foreach (string chore in chores)
+		{
+			Add(chore);
+		}
+	}
 
-    public void Add(string chore) => jsonObj.Add(chore, "Never");
+	public void Add(string chore) => jsonObj.Add(chore, "Never");
 
-    public void RemoveRange(string[] chores)
-    {
-        foreach (string chore in chores)
-        {
-            Remove(chore);
-        }
-    }
+	public void RemoveRange(string[] chores)
+	{
+		foreach (string chore in chores)
+		{
+			Remove(chore);
+		}
+	}
 
-    public void Remove(string chore)
-    {
-        if (jsonObj.ContainsKey(chore))
-        {
-            jsonObj.Remove(chore);
-            return;
-        }
-        Console.WriteLine("chore \"" + chore + "\" doesn't exist!");
-    }
+	public void Remove(string chore)
+	{
+		if (jsonObj.ContainsKey(chore))
+		{
+			jsonObj.Remove(chore);
+			return;
+		}
+		Console.WriteLine("chore \"" + chore + "\" doesn't exist!");
+	}
 
-    public void DoRange(string[] chores)
-    {
-        foreach (string chore in chores)
-        {
-            Do(chore);
-        }
-    }
+	public void DoRange(string[] chores)
+	{
+		foreach (string chore in chores)
+		{
+			Do(chore);
+		}
+	}
 
-    public void Do(string chore)
-    {
-        if (jsonObj.ContainsKey(chore))
-        {
-            jsonObj[chore] = DateTimeString;
-            return;
-        }
-        Console.WriteLine("chore \"" + chore + "\" doesn't exist!");
-    }
+	public void Do(string chore)
+	{
+		if (jsonObj.ContainsKey(chore))
+		{
+			jsonObj[chore] = DateTimeString;
+			return;
+		}
+		Console.WriteLine("chore \"" + chore + "\" doesn't exist!");
+	}
 
-    public const string HelpMessage =
-    """
-    list - List all your added chores with the last time you did them.
-    - Chores that you haven't done yet will display "Never" in them
-    add choreName1 choreName2 - Add new chores to your list
-    remove choreName1 choreName2 - Remove chores from your list
-    do choreName1 choreName2 - Do chores, setting the current time as their value
-    literally anything else - Display this help message
-    """;
+	public const string HelpMessage =
+	"""
+	list - List all your added chores with the last time you did them.
+	- Chores that you haven't done yet will display "Never" in them
+	add choreName1 choreName2 - Add new chores to your list
+	remove choreName1 choreName2 - Remove chores from your list
+	do choreName1 choreName2 - Do chores, setting the current time as their value
+	literally anything else - Display this help message
+	""";
 
-    public static void Help()
-    {
-        Console.WriteLine(StringExtensions.AlignByFirstChar(HelpMessage, '-'));
-    }
+	public static void Help()
+	{
+		Console.WriteLine(StringExtensions.AlignByFirstChar(HelpMessage, '-'));
+	}
 
-    public string Read()
-    {
-        JsonSerializerOptions jsonSerializerOptions = new() { WriteIndented = true };
-        return jsonObj.ToJsonString(jsonSerializerOptions);
-    }
+	public string Read()
+	{
+		JsonSerializerOptions jsonSerializerOptions = new() { WriteIndented = true };
+		return jsonObj.ToJsonString(jsonSerializerOptions);
+	}
 
-    public void SaveChanges()
-    {
-        File.WriteAllText(jsonPath, Read());
-    }
+	public void SaveChanges()
+	{
+		File.WriteAllText(jsonPath, Read());
+	}
 
-    public string GetListString()
-    {
-        StringBuilder sb = new();
-        string dateString;
-        foreach (KeyValuePair<string, JsonNode?> subObj in jsonObj)
-        {
-            if ((string)subObj.Value! == "Never")
-            {
-                dateString = "Never";
-            }
-            else
-            {
-                dateString = StringExtensions.GetDaysDifference((string)subObj.Value!) + " days ago";
-            }
-            sb.AppendLine(subObj.Key + " - " + dateString);
-        }
-        return StringExtensions.AlignByFirstChar(sb.ToString(), ' ');
-    }
+	public string GetListString()
+	{
+		StringBuilder sb = new();
+		string dateString;
+		foreach (KeyValuePair<string, JsonNode?> subObj in jsonObj)
+		{
+			if ((string)subObj.Value! == "Never")
+			{
+				dateString = "Never";
+			}
+			else
+			{
+				dateString = StringExtensions.GetDaysDifference((string)subObj.Value!) + " days ago";
+			}
+			sb.AppendLine(subObj.Key + " - " + dateString);
+		}
+		return StringExtensions.AlignByFirstChar(sb.ToString(), ' ');
+	}
 
-    public void List()
-    {
-        Console.WriteLine(GetListString());
-    }
+	public void List()
+	{
+		Console.WriteLine(GetListString());
+	}
 
-    private static JsonNode? DateTimeString => DateTime.Now.ToString("yyyy.MM.dd");
+	private static JsonNode? DateTimeString => DateTime.Now.ToString("yyyy.MM.dd");
 
-    private JsonObject Parse() => (JsonObject)JsonNode.Parse(File.ReadAllText(jsonPath))!;
+	private JsonObject Parse() => (JsonObject)JsonNode.Parse(File.ReadAllText(jsonPath))!;
 
-    private void MakeSureExists()
-    {
-        if (!File.Exists(jsonPath))
-        {
-            File.WriteAllText(jsonPath, "{}");
-        }
-    }
+	private void MakeSureExists()
+	{
+		if (!File.Exists(jsonPath))
+		{
+			File.WriteAllText(jsonPath, "{}");
+		}
+	}
 
 }
--- ChoreTracker/Program.cs
+++ ChoreTracker/Program.cs
@@ -5,67 +5,70 @@ namespace ChoreTracker;
 internal class Program
 {
 
-    private static readonly ChoreManager cm = new();
+	private static readonly ChoreManager cm = new();
 
-    private static void Main(string[] args)
-    {
-        Console.OutputEncoding = Encoding.Unicode;
-        Console.InputEncoding = Encoding.Unicode;
+	private static void Main(string[] args)
+	{
+		Console.OutputEncoding = Encoding.Unicode;
+		Console.InputEncoding = Encoding.Unicode;
 
-        string[]? arguments = ValidateArguments(args);
-        if (arguments is null)
-        {
-            return;
-        }
+		string[]? arguments = ValidateArguments(args);
+		if (arguments is null)
+		{
+			return;
+		}
 
-        DoAction(arguments);
+		DoAction(arguments);
 
-        cm.SaveChanges();
-    }
+		cm.SaveChanges();
+	}
 
-    private static string[]? ValidateArguments(string[] arguments)
-    {
+	private static string[]? ValidateArguments(string[] arguments)
+	{
 
-        if (arguments.Length != 0)
-        {
-            return arguments;
-        }
+		if (arguments.Length != 0)
+		{
+			return arguments;
+		}
 
-        Console.Write("Waiting for input: ");
-        string? input = Console.ReadLine();
-        if (string.IsNullOrEmpty(input))
-        {
-            return null;
-        }
+		Console.Write("Waiting for input: ");
+		string? input = Console.ReadLine();
+		if (string.IsNullOrEmpty(input))
+		{
+			return null;
+		}
 
-        return input.Split(' ');
-    }
+		return input.Split(' ');
+	}
 
-    private static void DoAction(string[] arguments)
-    {
+	private static void DoAction(string[] arguments)
+	{
 
-        string command = arguments[0].ToLower();
-        string[] options = arguments[1..];
+		string command = arguments[0].ToLower();
+		string[] options = arguments[1..];
 
-        switch (command)
-        {
+		switch (command)
+		{
 
-            case "add":
-                cm.AddRange(options);
-                break;
-            case "remove":
-                cm.RemoveRange(options);
-                break;
-            case "do":
-                cm.DoRange(options);
-                break;
-            case "list":
-                cm.List();
-                break;
-            default:
-                ChoreManager.Help();
-                break;
+			case "add":
+				cm.AddRange(options);
+				break;
+			case "remove":
+				cm.RemoveRange(options);
+				break;
+			case "do":
+				cm.DoRange(options);
+				break;
+			case "list":
+				cm.List();
+				break;
+			case "view":
+				cm.ViewRange(options);
+				break;
+			default:
+				ChoreManager.Help();
+				break;
 
-        }
-    }
+		}
+	}
 }
